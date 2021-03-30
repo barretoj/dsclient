4f5e7583e8aee400d8fb1b5dcf76ffa9b35f21e5
Jefferson Fernandes Barreto <jeffinho_fb@hotmail.com>
2021-03-30 20:48:55 -0300

    Final Project

---
+++ backend/src/main/java/com/devsuperior/dsclient/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.devsuperior.dsclient.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.devsuperior.dsclient.services.exceptions.DatabaseException;
+import com.devsuperior.dsclient.services.exceptions.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(getStandardError(e, request, HttpStatus.NOT_FOUND));
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(ResourceNotFoundException e, HttpServletRequest request) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(getStandardError(e, request, HttpStatus.NOT_FOUND));
+	}
+	
+	private StandardError getStandardError(Exception e, HttpServletRequest request, HttpStatus status) {
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Database exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return err;
+	}
+	
+}
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Jefferson Barreto', '1234567890', 4500.0, '1988-08-20T22:30:54Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Vanda Fernandes', '2345678901', 10000.0, '1960-07-09T09:10:54Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Jose Barros', '012345678', 15000.0, '1951-11-13T09:30:54Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Camila Alves', '57951354618', 1500.0, '1999-01-01T00:00:54Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Pedro Vaz', '02468134892', 1000.0, '1945-02-15T10:20:54Z', 5);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Marcos Liz', '44444455566', 4000.0, '1975-08-13T09:30:54Z', 4);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Luiz Carlos', '78542635186', 3123.0, '1998-04-10T12:30:54Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Ana Maria', '99977788866', 20000.0, '1955-12-10T01:00:44Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Luciano Abrel', '01222233345', 17500.0, '1969-01-24T20:20:54Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Maria de Loures', '85296374102', 5000.0, '1972-06-07T11:23:00Z', 1);
